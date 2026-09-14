d9200d22c7807aacef5b13fa7a4491bd8db9e0df
ankara koltuk yıkama <empatiseo@gmail.com>
2026-09-14 16:23:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def publish_to_blogger(title, content, labels=None, meta_description=None):
         print("Haber yayınlanamadı:", r.status_code, r.text)
 
 if __name__ == "__main__":
-    topics = ["Ekonomi", "Spor", "Teknoloji", "Sağlık"]
+    topics = ["Ekonomi", "Futbol", "Basketbol", "Voleybol", "Bilim", "Sanat", "Kültür", "Teknoloji", "Sağlık"]
 
     for topic in topics:
         print(f"\n--- {topic} için haber üretiliyor ---")
